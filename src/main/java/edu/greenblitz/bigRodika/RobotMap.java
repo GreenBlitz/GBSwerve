c2de0df38a78a6857cbfc357d5f94493b83f5d80
unknown <greenblitz.4590@gmail.com>
2021-12-29 21:01:58 +0200

    asaf&nitzan - made code compatable with one spark max and pid is mostly finished still non deterministic

--- src/main/java/edu/greenblitz/bigRodika/RobotMap.java
+++ src/main/java/edu/greenblitz/bigRodika/RobotMap.java
@@ -60,7 +60,7 @@ public class RobotMap {
                 public static final double TICKS_PER_ROTATION = 42;
                 public static final double TICKS_TO_METERS = WHEEL_PERIMETER / TICKS_PER_ROTATION; // TODO: insert gear conversion in swerve module to constant
                 public static final double DRIVE_GEAR_RATIO = 0.125;
-                public static final double ROTATION_GEAR_RATIO = 1.0/6;
+                public static final double ROTATION_GEAR_RATIO = 1.0 / 6;
 
 
                 public static final int[] DRIVE_MOTOR_PORTS = {4, 1, 2, 3};
@@ -72,34 +72,32 @@ public class RobotMap {
             }
 
             public static class SwerveModule {
-                public static final double DRIVE_P = 0, DRIVE_I = 0, DRIVE_D = 0,
-                        ANGLE_P = 0.5, ANGLE_I = 0, ANGLE_D = 2;
-                public static final GearDependentValue<Double> NORMALIZER_SPARK = new GearDependentValue<Double>(42.0,
-                        2048.0); // TODO: GearDependentValues are deprecated, should be a constant value.
+                public static final double DRIVE_P = 0, DRIVE_I = 0, DRIVE_D = 0, ANGLE_P = 0.3, ANGLE_I = 0, ANGLE_D = 2;
+                public static final GearDependentValue<Double> NORMALIZER_SPARK = new GearDependentValue<Double>(42.0, 2048.0); // TODO: GearDependentValues are deprecated, should be a constant value.
                 public static Dataset SPEED_TO_FF = new Dataset(2);
 
                 static {
-                    SwerveModule.SPEED_TO_FF.addDatapoint(-0.000000000000001,new double[]{0.0});
-                    SwerveModule.SPEED_TO_FF.addDatapoint(0.15874853129485736,new double[]{0.05});
-                    SwerveModule.SPEED_TO_FF.addDatapoint(0.36219983433057434,new double[]{0.1});
-                    SwerveModule.SPEED_TO_FF.addDatapoint(0.7855288332582557,new double[]{0.2});
-                    SwerveModule.SPEED_TO_FF.addDatapoint(1.1745280432855925,new double[]{0.3});
-                    SwerveModule.SPEED_TO_FF.addDatapoint(1.5679483142460156,new double[]{0.4});
-                    SwerveModule.SPEED_TO_FF.addDatapoint(2.018132357679125,new double[]{0.5});
-                    SwerveModule.SPEED_TO_FF.addDatapoint(2.4253292179198764,new double[]{0.6});
-                    SwerveModule.SPEED_TO_FF.addDatapoint(2.841689838182813,new double[]{0.7});
-                    SwerveModule.SPEED_TO_FF.addDatapoint(3.153622487669066,new double[]{0.8});
-                    SwerveModule.SPEED_TO_FF.addDatapoint(3.6464959043498486,new double[]{0.9});
-                    SwerveModule.SPEED_TO_FF.addDatapoint(4.049262404948307,new double[]{1});
+                    SwerveModule.SPEED_TO_FF.addDatapoint(-0.000000000000001, new double[]{0.0});
+                    SwerveModule.SPEED_TO_FF.addDatapoint(0.15874853129485736, new double[]{0.05});
+                    SwerveModule.SPEED_TO_FF.addDatapoint(0.36219983433057434, new double[]{0.1});
+                    SwerveModule.SPEED_TO_FF.addDatapoint(0.7855288332582557, new double[]{0.2});
+                    SwerveModule.SPEED_TO_FF.addDatapoint(1.1745280432855925, new double[]{0.3});
+                    SwerveModule.SPEED_TO_FF.addDatapoint(1.5679483142460156, new double[]{0.4});
+                    SwerveModule.SPEED_TO_FF.addDatapoint(2.018132357679125, new double[]{0.5});
+                    SwerveModule.SPEED_TO_FF.addDatapoint(2.4253292179198764, new double[]{0.6});
+                    SwerveModule.SPEED_TO_FF.addDatapoint(2.841689838182813, new double[]{0.7});
+                    SwerveModule.SPEED_TO_FF.addDatapoint(3.153622487669066, new double[]{0.8});
+                    SwerveModule.SPEED_TO_FF.addDatapoint(3.6464959043498486, new double[]{0.9});
+                    SwerveModule.SPEED_TO_FF.addDatapoint(4.049262404948307, new double[]{1});
+                }
             }
         }
-    }
 
-    public static class Measures {
-        public static final double LENGTH = 1.5;
-        public static final double WIDTH = 1;
-        public static final double WHEEL_DIST_FROM_CENTER = Math.sqrt(Math.pow(LENGTH, 2) + Math.pow(WIDTH, 2));
+        public static class Measures {
+            public static final double LENGTH = 1.5;
+            public static final double WIDTH = 1;
+            public static final double WHEEL_DIST_FROM_CENTER = Math.sqrt(Math.pow(LENGTH, 2) + Math.pow(WIDTH, 2));
+        }
     }
-}
 }
 
