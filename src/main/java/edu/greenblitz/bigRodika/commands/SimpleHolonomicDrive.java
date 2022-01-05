a3787f11a7e958ea5331d2fb88234bd3be101d1c
unknown <greenblitz.4590@gmail.com>
2022-01-05 16:42:07 +0200

    asaf- added better holonomicDrive that is by wheel and set as default command

---
+++ src/main/java/edu/greenblitz/bigRodika/commands/SimpleHolonomicDrive.java
@@ -0,0 +1,49 @@
+package edu.greenblitz.bigRodika.commands;
+
+import edu.greenblitz.bigRodika.exceptions.MotorPowerOutOfRangeException;
+import edu.greenblitz.bigRodika.subsystems.Chassis;
+import edu.greenblitz.gblib.hid.SmartJoystick;
+
+/**
+ * @author Orel
+ */
+
+public class SimpleHolonomicDrive extends ChassisCommand {
+
+    private final SmartJoystick joystick;
+    private final Chassis chassis;
+    private final double POWER_CONST = 1.0;
+    private boolean fieldOriented = true;
+
+    public SimpleHolonomicDrive(SmartJoystick joystick){
+        this.joystick = joystick;
+        chassis = Chassis.getInstance();
+    }
+
+    @Override
+    public void initialize(){
+        chassis.toBrake();
+    }
+
+    @Override
+    public void execute() {
+        double xVal = joystick.getAxisValue(SmartJoystick.Axis.LEFT_X);
+        double yVal = joystick.getAxisValue(SmartJoystick.Axis.LEFT_Y);
+        double power = getLinearPower(xVal, yVal);
+        double angle = getDriveAngle(xVal, yVal);;
+        try {
+            chassis.moveMotors(new double[]{power, power, power, power}, new double[]{angle, angle, angle, angle});
+        } catch (MotorPowerOutOfRangeException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public double getLinearPower(double xVal, double yVal){
+        return Math.sqrt(Math.pow(xVal, 2) + Math.pow(2, yVal));
+    }
+
+    public double getDriveAngle(double xVal, double yVal){
+        return Math.atan(yVal/xVal);
+    }
+
+}
